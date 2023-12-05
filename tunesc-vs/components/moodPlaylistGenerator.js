15a2ccaf361635bc6f2c4488d75c3036463d8dc2
ivanpaulcodes <thetwelfthman15@gmail.com>
2023-12-05 23:35:08 +0800

    ayoko na

--- tunesc-vs/components/moodPlaylistGenerator.js
+++ tunesc-vs/components/moodPlaylistGenerator.js
@@ -1,96 +1,99 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const moodForm = document.getElementById('moodForm');
-  
-    moodForm.addEventListener('submit', function(event) {
-      event.preventDefault(); // Prevent the form from submitting the traditional way
-      const selectedMood = document.getElementById('moodDropdown').value;
-      generatePlaylistForMood(selectedMood);
-    });
+function initializeApp() {
+  // Extract the access token from the URL
+  getAccessTokenFromUrl();
+
+  // Add event listeners or other initialization logic here
+  const moodForm = document.getElementById('moodForm');
+  moodForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const selectedMood = document.getElementById('moodDropdown').value;
+    generatePlaylistForMood(selectedMood);
   });
+}
+
+
+// Initialize the application when the page is fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+  const moodForm = document.getElementById('moodForm');
   
-  function generatePlaylistForMood(mood) {
-    fetchSpotifyUserID().then(userId => {
-      const attributes = mapMoodToAttributes(mood);
-      fetchTracksWithAttributes(attributes).then(tracks => {
-        if (tracks.length > 0) {
-          createOrUpdatePlaylist(userId, tracks);
-        } else {
-          alert('No tracks found for this mood. Please try a different mood.');
-        }
-      }).catch(error => {
-        console.error('Error fetching tracks:', error);
-        alert('An error occurred while generating the playlist.');
-      });
+  // Call the function from spot.js to ensure the access token is retrieved from the URL
+  if (typeof getAccessTokenFromUrl === "function") { 
+    getAccessTokenFromUrl();
+  }
+
+  moodForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const selectedMood = document.getElementById('moodDropdown').value;
+    generatePlaylistForMood(selectedMood);
+  });
+});
+
+function fetchSpotifyUserID() {
+  // The access token is used here to fetch the Spotify user ID
+  return fetch('https://api.spotify.com/v1/me', {
+    headers: {
+      'Authorization': `Bearer ${window.access_token}`
+    }
+  })
+  .then(response => response.json())
+  .then(data => data.id);
+}
+
+function mapMoodToAttributes(mood) {
+  // Maps the selected mood to track attributes used in the Spotify API
+  return {
+    valence: mood === 'happy' ? 0.8 : mood === 'sad' ? 0.2 : 0.5,
+    energy: mood === 'energetic' ? 0.8 : 0.4,
+    // ...other mood mappings...
+  };
+}
+
+function fetchTracksWithAttributes(attributes) {
+  // Fetch tracks from Spotify based on the attributes associated with the selected mood
+  return fetch(`https://api.spotify.com/v1/recommendations?seed_genres=pop&target_valence=${attributes.valence}&target_energy=${attributes.energy}`, {
+    headers: {
+      'Authorization': `Bearer ${window.access_token}`
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    return data.tracks; // Return the full track objects
+  });
+}
+
+function generatePlaylistForMood(mood) {
+  fetchSpotifyUserID().then(userId => {
+    const attributes = mapMoodToAttributes(mood);
+    fetchTracksWithAttributes(attributes).then(tracks => {
+      if (tracks.length > 0) {
+        displayTracksAsText(tracks);
+        // Optionally create a Spotify playlist with these tracks
+        // createOrUpdatePlaylist(userId, tracks.map(track => track.uri));
+      } else {
+        alert('No tracks found for this mood. Please try a different mood.');
+      }
     }).catch(error => {
-      console.error('Error fetching user ID:', error);
+      console.error('Error fetching tracks:', error);
+      alert('An error occurred while generating the playlist.');
     });
-  }
-  
-  function fetchSpotifyUserID() {
-    // Replace 'access_token' with the actual access token
-    return fetch('https://api.spotify.com/v1/me', {
-      headers: {
-        'Authorization': `Bearer ${access_token}`
-      }
-    })
-    .then(response => response.json())
-    .then(data => data.id);
-  }
-  
-  function mapMoodToAttributes(mood) {
-    // Map the mood to Spotify track attributes
-    // Replace with actual logic based on Spotify's API
-    return {
-      valence: mood === 'happy' ? 0.8 : mood === 'sad' ? 0.2 : 0.5,
-      energy: mood === 'energetic' ? 0.8 : 0.4,
-      // Add other attributes if needed
-    };
-  }
-  
-  function fetchTracksWithAttributes(attributes) {
-    // Replace with actual Spotify API call
-    return fetch(`https://api.spotify.com/v1/recommendations?seed_genres=pop&target_valence=${attributes.valence}&target_energy=${attributes.energy}`, {
-      headers: {
-        'Authorization': `Bearer ${access_token}` // Using access_token from spot.js
-      }
-    })
-    .then(response => response.json())
-    .then(data => {
-      return data.tracks.map(track => track.uri);
-    });
-  }
-  
-  function createOrUpdatePlaylist(userId, tracks) {
-    // Replace with actual Spotify API call for playlist creation/updating
-    return fetch(`https://api.spotify.com/v1/users/${userId}/playlists`, {
-      method: 'POST',
-      headers: {
-        'Authorization': `Bearer ${access_token}`, // Using access_token from spot.js
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name: `Mood Playlist: ${new Date().toLocaleDateString()}`, // Playlist name with current date
-        description: 'Generated by TuneSculpt',
-        public: false
-      })
-    })
-    .then(response => response.json())
-    .then(playlist => {
-      return fetch(`https://api.spotify.com/v1/playlists/${playlist.id}/tracks`, {
-        method: 'POST',
-        headers: {
-          'Authorization': `Bearer ${access_token}`, // Using access_token from spot.js
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ uris: tracks })
-      });
-    })
-    .then(() => {
-      alert('Playlist generated successfully!');
-    })
-    .catch(error => {
-      console.error('Error creating or updating playlist:', error);
-      alert('An error occurred while creating the playlist.');
-    });
-  }
-  
+  }).catch(error => {
+    console.error('Error fetching user ID:', error);
+  });
+}
+
+function displayTracksAsText(tracks) {
+  const trackListContainer = document.getElementById('trackListContainer'); // Make sure this exists in your HTML
+  trackListContainer.innerHTML = ''; // Clear existing track list
+
+  tracks.forEach(track => {
+    const trackElement = document.createElement('p');
+    trackElement.textContent = `Track: ${track.name} by ${track.artists.map(artist => artist.name).join(', ')}`;
+    trackListContainer.appendChild(trackElement);
+  });
+}
+
+
+function createOrUpdatePlaylist(userId, trackUris) {
+  // Create a new playlist and add the tracks to it
+  // ...
+}
--- tunesc-vs/components/spot.js
+++ tunesc-vs/components/spot.js
@@ -1,21 +1,24 @@
+// spot.js content
 const client_id = "3298c0aed7f940dab9fc68c1d0bbc812";
-const client_secret = "4dcdf67d414b4f0192db89958fa16108";
 const redirect_uri = "http://127.0.0.1:5500/tunesc-vs/pages/Dashboard.html";
 const AUTHORIZE = "https://accounts.spotify.com/authorize";
-const scopes =
-  "user-read-private user-read-email playlist-modify-public playlist-modify-public ugc-image-upload";
+const scopes = "user-read-private user-read-email playlist-modify-public ugc-image-upload";
 
-//gets token sa urlbar
-const urlParams = new URLSearchParams(window.location.hash.substr(1));
-const access_token = urlParams.get("access_token");
+// Function to parse the access token from the URL hash
+function getAccessTokenFromUrl() {
+  const hash = window.location.hash.substr(1);
+  const urlParams = new URLSearchParams(hash);
+  window.access_token = urlParams.get("access_token");
+}
 
+// Function to initiate the Spotify authorization process
 function authorizeSpot() {
   let url = AUTHORIZE;
   url += "?response_type=token";
-  url += "&client_id=" + client_id;
-  url += "&scope=" + scopes;
-  url += "&redirect_uri=" + encodeURI(redirect_uri);
-  //   url += "&state=" + encodeURIComponent(state);
+  url += "&client_id=" + encodeURIComponent(client_id);
+  url += "&scope=" + encodeURIComponent(scopes);
+  url += "&redirect_uri=" + encodeURIComponent(redirect_uri);
   url += "&show_dialog=true";
   window.location.href = url;
 }
+
